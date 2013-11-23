6f75cec3eb693c52a7550462593546450c57833a
Vlad <vcdesinger@gmail.com>
2013-11-23 03:21:48 +0100

    1212

--- index.php
+++ index.php
@@ -11,6 +11,6 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 /*
- * Влад Чернецкий 21-->
+ * 1321321312312312312312312
  */
 Yii::createWebApplication($config)->run();
